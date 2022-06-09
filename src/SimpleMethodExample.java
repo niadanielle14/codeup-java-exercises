e1cd766dc71c94a9eefecc43236b12d7e7a5de67
Nia Watson <nia.d.watson@gmail.com>
2022-06-09 15:41:35 -0500

    method Exercises

---
+++ src/SimpleMethodExample.java
@@ -0,0 +1,16 @@
+public class SimpleMethodExample {
+    public static int doMath(int num1, int num2, int num3) {
+        return num1 + (num2 * num3);
+    }
+    public static void main(String[] args) {
+
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+
+        int total = doMath(num1, num2, num3);
+
+        System.out.println(total);
+
+    }
+}
