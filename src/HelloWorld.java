9c2c17c53e1cba64832fb01120211ec6f3e9fca4
Nia Watson <nia.d.watson@gmail.com>
2022-06-16 10:46:28 -0500

    Syntax, Variable, Data Types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFavoriteNumber = 26;
+        System.out.println(myFavoriteNumber);
+
+        String niaWord = "Hello World, how are you?";
+        System.out.println(niaWord);
     }
 }
--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -1,10 +1,10 @@
-public class MethodExercises {
-    public static void main(String[] args) {
-        System.out.println(add(1,2));
-        System.out.println(subtract(4,2));
-        System.out.println(multiply(10,2));
-        System.out.println(divide(10,2));
-        System.out.println(module(10,2));
-        ;
-    }
-}
+//public class MethodExercises {
+//    public static void main(String[] args) {
+//        System.out.println(add(1,2));
+//        System.out.println(subtract(4,2));
+//        System.out.println(multiply(10,2));
+//        System.out.println(divide(10,2));
+//        System.out.println(module(10,2));
+//        ;
+//    }
+//}
