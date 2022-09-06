5e3903c4111064310383b51bf1e74144909acbb9
Nia Watson <nia.d.watson@gmail.com>
2022-09-06 13:59:21 -0500

    feat. changed the Hello world

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,10 +2,14 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Measurable myShape = new Rectangle(5,3);
+        Measurable myShape = new Rectangle(5, 3);
         double perimeter = myShape.getPerimeter();
         System.out.println(perimeter);
 
-        Measurable myShape2 = new Square(5,3);
+        Measurable myShape2 = new Square(5, 3);
         double area = myShape2.getArea();
         System.out.println(area);
+
+    }
+
+}
